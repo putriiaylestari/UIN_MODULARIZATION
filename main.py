44c2a19eb6c2cbaf5fd854ded7ab8a770189b005
putriiaylestari <56154724+putriiaylestari@users.noreply.github.com>
2019-10-11 16:44:00 +0700

    kecepatan tanpa fugsi

---
+++ main.py
@@ -0,0 +1,11 @@
+nama = 'Putri Ayu Lestari'
+program = 'Gerak Lurus'
+
+print(f'program (program) oleh {nama}')
+
+jarak = 1000
+waktu = 5 * 60
+kecepatan = jarak / waktu
+
+print (f'jarak = {jarak/1000}km ditempuh dalam waktu = {waktu/60}menit')
+print(f'sehingga kecepatan = {kecepatan} m/s')

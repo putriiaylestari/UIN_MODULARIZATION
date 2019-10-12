17591c81b55775cf3ce036c000db829ad0a23534
putriiaylestari <56154724+putriiaylestari@users.noreply.github.com>
2019-10-12 23:42:51 +0700

    fungsi usaha

--- main.py
+++ main.py
@@ -14,4 +14,13 @@ def hitung_kecepatan(jarak, waktu) :
 kecepatan = hitung_kecepatan(1000, 5 * 60)
 kecepatan = hitung_kecepatan(3000, 70 * 60)
 
+def hitung_usaha(gaya, jarak ) :
+    usaha = gaya * jarak
+    print(f'gaya = {gaya / 10}Newton dikenai sebuah perpindahan = {jarak / 1}meter')
+    print(f' sehingga usaha = {usaha} joule')
+    return usaha
 
+# gaya
+# jarak
+usaha = hitung_usaha(10, 2 * 1)
+usaha = hitung_usaha(10, 10 * 1)
